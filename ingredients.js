0da6d117e19cb11ac7e588435f91760a79507987
nowhed <50598926+nowhed@users.noreply.github.com>
2021-03-19 12:13:16 -0400

    Update ingredients.js

--- ingredients.js
+++ ingredients.js
@@ -1,15 +1,15 @@
 var ingredients = [
     ["Bread", 1.15, 0.35],
     ["Cheese", 2, 0.50],
-    ["Bacon",1,1.5],
-    ["Jelly",2.66,3.15]
+    ["Turkey", 1.5, 1],
+    ["Lettuce",2.83,1.4]
 ]
 var ingredientBank = [
-    ["Turkey", 1.5, 1],
+    ["Bacon",1,1.5],
     ["Tomato", 2, 0.50],
     ["Fried Egg",3,0.2],
-    ["Lettuce",2.83,1.4],
-    ["Peanut Butter",3.87,2.7]
+    ["Jelly",2.5,1],
+    ["Peanut Butter",1,2.5]
 ]
 function getIngredient(ingrNumber){
     ingredients.unshift(ingredientBank[Math.round(ingrNumber)]); //gets ingr from bank, puts into ingredients
