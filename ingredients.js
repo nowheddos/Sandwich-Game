0df43b21f793f259e327126a8c33a8e4575beda6
nowhed <50598926+nowhed@users.noreply.github.com>
2021-03-19 11:09:17 -0400

    dumb bug

--- ingredients.js
+++ ingredients.js
@@ -4,7 +4,7 @@ var ingredients = [
     ["Bacon",1,1.5],
     ["Fried Egg",3,0.2],
     ["Lettuce",2.83,1.4],
-    ["Peanut Butter",3.87,2.7]
+    ["Peanut Butter",3.87,2.7],
     ["Jelly",2.66,3.15]
 ]
 var ingredientBank = [
