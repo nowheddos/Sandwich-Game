12a0481b61bd13968bffdba1ea25f1d6faac9eb8
nowhed <50598926+nowhed@users.noreply.github.com>
2021-03-20 00:36:15 -0400

    Update milestones.js

--- milestones.js
+++ milestones.js
@@ -4,15 +4,18 @@ function gameStageRender() { //game unlocks
 	document.getElementById("logDiv").className = "column";
 	document.getElementById("currentlyStats").className = "column";
     document.getElementById("shopSection").className = "column";
+	document.getElementById("Research").className = "column";
 	switch(gameStage){ //i am so terribly sorry for this piece of trash
         case 4:
             milestone("Game finished! Expect more later :)");
             document.getElementById("alertsBox").innerHTML = "Shop unlocked! I wonder what's next...";
+			document.getElementById("Research").className = "invisible";
             break;
 		case 3:
 			milestone("$25.00")
 			document.getElementById("alertsBox").innerHTML = "Reaching $25 would get you a shop. Or so they say.";
 			document.getElementById("shopSection").className = "invisible";
+			document.getElementById("Research").className = "invisible";
             console.log("3")
 			break;
 		case 2:
@@ -20,6 +23,7 @@ function gameStageRender() { //game unlocks
 			document.getElementById("recipeBookSection").className = "invisible";
 			document.getElementById("alertsBox").innerHTML = "Would be nice to have $15 for a recipe book..";
 			document.getElementById("shopSection").className = "invisible";
+			document.getElementById("Research").className = "invisible";
             console.log("2")
 			break;
 		case 1:
@@ -28,6 +32,7 @@ function gameStageRender() { //game unlocks
 			document.getElementById("recipeBookSection").className = "invisible";
 			document.getElementById("logDiv").className = "invisible";
             document.getElementById("shopSection").className = "invisible";
+			document.getElementById("Research").className = "invisible";
 			break;
 		case 0:
 			milestone("$5.50")
@@ -36,6 +41,7 @@ function gameStageRender() { //game unlocks
 			document.getElementById("logDiv").className = "invisible";
 			document.getElementById("currentlyStats").className = "invisible";
             document.getElementById("shopSection").className = "invisible";
+			document.getElementById("Research").className = "invisible";
 			console.log("0")
 	}
 }
